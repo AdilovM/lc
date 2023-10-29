7075b33f279f65dea91408e49ef36df6c3ec9000
Miras Adilov <miras9191@gmail.com>
2023-10-28 21:33:41 -0700

    Time: 3639 ms (66.67%), Space: 16.5 MB (39.28%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.py
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.py
@@ -1,42 +1,20 @@
 class Solution:
     def largestVariance(self, s: str) -> int:
-        chars = {}
-        for c in s:
-            chars[c] = chars.get(c, 0) + 1
+        f1 = 0
+        f2 = 0
+        max_var = 0
+        pairs = [(l1, l2) for l1 in set(s) for l2 in set(s) if l1 != l2]
 
-        permutations = itertools.permutations(chars, 2)
-        count = 0
-        for a, b in permutations:
-            count = max(self.kadene(a, b, s, chars), count)
-        return count
-
-    def kadene(self, a, b, s, chars):
-        count = 0
-        max_local = 0
-        is_a = False
-        is_b = False
-
-        val_a = chars[a]
-        val_b = chars[b]
-        for c in s:			
-            if c != a and c != b:
-                continue
-
-            if max_local < 0 and val_a != 0 and val_b != 0:
-                max_local = 0
-                is_a = False
-                is_b = False
-
-            if c == a:
-                max_local += 1
-                val_a -= 1
-                is_a = True
-						
-            if c == b:
-                max_local -= 1
-                val_b -=1
-                is_b = True
-            
-            if is_a and is_b:
-                count = max(count, max_local)
-        return count
+        for _ in range(2):
+            for pair in pairs:
+                f1 = f2 = 0
+                for letter in s:
+                    if letter not in pair: continue
+                    if letter == pair[0]: f1 += 1
+                    elif letter == pair[1]: f2 += 1
+                    
+                    if f1 < f2: f1 = f2 = 0
+                    elif f1 > 0 and f2 > 0:
+                            max_var = max(max_var, f1 - f2)
+            s = s[::-1]
+        return max_var
