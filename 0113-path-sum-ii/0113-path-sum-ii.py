0f6aeec8b529f475a014890ba141d8d2a415de39
Miras Adilov <miras9191@gmail.com>
2023-11-01 14:22:44 -0700

    Time: 45 ms (77.97%), Space: 18 MB (69.04%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.py
+++ 0113-path-sum-ii/0113-path-sum-ii.py
@@ -7,20 +7,30 @@
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
         
-        def helper_dfs( node, current_path, current_sum):
+        def helper_dfs(node, current_path, current_sum):
+            # if no more nodes, return back to recursive call
             if not node:
                 return
+            # add all nodes on same path
             current_path.append(node.val)
+            
+            # increment current_sum by node.val
             current_sum += node.val
-
+            
+            # if we are on leaves and if node vals/current_sum we traversed so far addes up to targetSum copy current_path into 
+            # all_pathes bc that is one of root-to-leaf path that gives targetSum
             if not node.left and not node.right and current_sum == targetSum:
-                result.append(list(current_path))
-
+                all_pathes.append(list(current_path))
+        
+            # do the same for left and right children
             helper_dfs(node.left, current_path, current_sum)
             helper_dfs(node.right, current_path, current_sum)
-
+            
+            # since we've been adding all node vals into current_path and copied that current_path into all_pathes we need to reset/empty current_path
             current_path.pop()
-
-        result = []
+            
+        
+        all_pathes = []
         helper_dfs(root, [], 0)
-        return result
+        return all_pathes
+        
