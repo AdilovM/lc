3db4396d9adc42f3d54815feeb37183fce22daaa
Miras Adilov <miras9191@gmail.com>
2023-11-01 18:58:20 -0700

    Time: 88 ms (15.82%), Space: 23.7 MB (89.63%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -7,18 +7,27 @@
 class Solution:
     
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        answer = float('-inf')
+        """
+        1. using helper dfs return max value between (left child max sum and right child max sum) + parent node value
+        2. max sum value is max value between what helper dfs returns and zero bc if child's max sum is negative we should not add it to global max sum value
+        3. global max is max between current global max and sum of children values and current node value
+        4. assign nonlocal variable answer that represents final answer
+        5. if given node is null then return 0
+        """
+        
+        global_max_sum = float('-inf')
+        
         def helper_dfs(node):
-            nonlocal answer
+            nonlocal global_max_sum
             if not node:
                 return 0
             
-            max_left_path = max(helper_dfs(node.left), 0)
-            max_right_path = max(helper_dfs(node.right), 0)
+            left_path_max_sum = max(helper_dfs(node.left), 0)
+            right_path_max_sum = max(helper_dfs(node.right), 0)
             
-            answer = max(answer, max_left_path + max_right_path + node.val)
+            global_max_sum = max(global_max_sum,left_path_max_sum + right_path_max_sum + node.val)
             
-            return max(max_left_path, max_right_path) + node.val
+            return max(left_path_max_sum, right_path_max_sum) + node.val
         
         helper_dfs(root)
-        return answer
+        return global_max_sum
