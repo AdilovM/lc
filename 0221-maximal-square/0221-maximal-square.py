b3f35a8ab8e9e629dc78b0e852cc7f219fafeb3f
Miras Adilov <miras9191@gmail.com>
2023-11-04 21:09:45 -0700

    Time: 608 ms (56.11%), Space: 19.3 MB (35.97%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -2,12 +2,16 @@ class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
         # dynamic programming bottom up
         # recursive: top down
+        if not matrix or not matrix[0]:
+            return 0
         H = len(matrix)
         W = len(matrix[0])
+        # create a cache
         dp = [[0 for _ in range(W)] for _ in range(H)]
         answer = 0
         for row in range(H):
             for col in range(W):
+                
                 if matrix[row][col] == "1":
                     dp[row][col] = 1
                     if row > 0 and col > 0:
