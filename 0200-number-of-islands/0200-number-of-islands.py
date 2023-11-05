ba1a2923fe3f961c787b4c437d97257e5259d589
Miras Adilov <miras9191@gmail.com>
2023-11-04 17:48:45 -0700

    Time: 375 ms (7.77%), Space: 24.3 MB (24.21%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,47 +1,33 @@
 class Solution:
-  def numIslands(self, grid: List[List[str]]) -> int:
-    if not grid:
-        return 0
-    
-    # dimensions of grid
-    rows, cols = len(grid), len(grid[0])
-    # mark visited positions
-    visited = set()
-    # count num of islands
-    islands = 0
-    
-    # bfs is not recursive, it is iterative so we need ds to use for memory. I'll use a queue
-    def bfs(r,c):
-        q = collections.deque()
-        # mark as visited
-        visited.add((r,c))
-        # add to queue
-        q.append((r,c))
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        rows, cols = len(grid), len(grid[0])
+        visited = set()
+        islands = 0
         
-        # while queue is not empty I'll continue expanding the island
-        while q:
-            row, col = q.popleft()
-            directions = [[1,0], [-1, 0], [0, 1], [0,-1]]
+        def bfs(row, col):
+            q = collections.deque()
+            q.append((row, col))
+            visited.add((row, col))
             
-            for dr, dc in directions:
-                r, c = row + dr, col + dc
-                if r in range(rows) and c in range(cols) and grid[r][c] == "1" and (r, c) not in visited:
+            while q:
+                row, col = q.popleft()
+                directions = [[1,0], [-1,0], [0,1], [0,-1]]
                 
-                    # if that's true we need to run bfs on given cell
-                    q.append((r, c))
-                    visited.add((r,c))
-
-    
-    # every position in grid
-    for r in range(rows):
-        for c in range(cols):
-            # if visited "1"
-            if grid[r][c] == "1" and (r,c) not in visited:
-                bfs(r,c)
-                islands += 1
-    return islands
-
-
-
-
-           
+                for dr, dc in directions:
+                    r, c = row + dr, col + dc
+                    if r in range(rows) and c in range(cols) and (r,c) not in visited and grid[r][c] == "1":
+                        q.append((r,c))
+                        visited.add((r,c))
+        
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == "1" and (row, col) not in visited:
+                    bfs(row, col)
+                    islands += 1
+        return islands
+        
+        
+        
+        
