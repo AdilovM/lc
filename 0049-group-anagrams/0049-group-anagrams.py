6ded72933eee7df44fbcb78d918f498a9bdaa823
Miras Adilov <miras9191@gmail.com>
2023-11-06 19:54:17 -0800

    Time: 105 ms (39.19%), Space: 21.4 MB (31.07%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,9 @@
 class Solution:
   def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-    result = {}
-    for word in strs:
-        letter_count = [0 for _ in  range(26)] #array with length of alphabet
-        for letter in word:
-            letter_count[ord(letter)-97] += 1
-        listhash = ''.join((str(l)+" ") for l in letter_count)
-        print(listhash)
-        if listhash not in result:
-            result[listhash] = []
-        result[listhash].append(word)
-    return result.values()
+    result = defaultdict(list)
+    for s in strs:
+      count = [0] * 26
+      for c in s:
+        count[ord(c) - ord("a")] += 1
+      result[tuple(count)].append(s)
+    return list(result.values())
