54fe3cfe8ff581f5f29c1998a20ec6b34c0a6939
Miras Adilov <miras9191@gmail.com>
2023-11-13 12:56:13 -0800

    Time: 103 ms (45.66%), Space: 22.2 MB (9.79%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,9 +1,17 @@
 class Solution:
-  def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-    result = defaultdict(list)
-    for s in strs:
-      count = [0] * 26
-      for c in s:
-        count[ord(c) - ord("a")] += 1
-      result[tuple(count)].append(s)
-    return list(result.values())
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        """
+        create a list of 26 zeros.
+        each index represents order of given chars. a is 0, b = 1 etc. index is calculated as ord(char) - ord('a') + 1
+        each word is represented as list of zeros and ones. anagram words will be mapped to same list using defaultdict
+        return values of defaultdict
+        """
+        result = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            result[tuple(count)].append(s)
+        return result.values()
+                
+        
