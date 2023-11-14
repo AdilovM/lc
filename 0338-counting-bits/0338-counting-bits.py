2841c8209ae1058466fffdf5e4fc400400f9afc1
Miras Adilov <miras9191@gmail.com>
2023-11-14 12:08:23 -0800

    Time: 72 ms (67.32%), Space: 23.2 MB (30.26%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -3,10 +3,9 @@ class Solution:
         # n+1 as we are going to count from 0 to n
         num_bits = [0] * (n + 1)
         # num_bits[0] will be 0 because 0 has count of set bit is 0
-        num_bits[0] = 0
         # we can compute current set bit count using previous count
         # as n/2 in O(1) time
         # i%2 will be 0 for even number ans 1 for odd number
         for i in range(1, n + 1):
-            num_bits[i] = num_bits[int(i/2)] + i%2
+            num_bits[i] = num_bits[int(i//2)] + i%2
         return num_bits
