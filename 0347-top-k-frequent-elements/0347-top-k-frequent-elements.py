ec51c9b54a4a6e45c7898c51937c668ea0b8081c
Miras Adilov <miras9191@gmail.com>
2023-11-13 16:13:07 -0800

    Time: 108 ms (19.76%), Space: 21.2 MB (38.34%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,17 +1,9 @@
-from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        """
-        1.map number to its count
-        2.bucket (list of lists) where index for num is num's count and inner list consits of nums mapped to that count
-        3.traverse the bucket backwards to return the K most freq elements
-        """
         counts = {}
         for num in nums:
             counts[num] = counts.get(num, 0) + 1
-        freq_list = [(number, freq) for number, freq in counts.items()]
-        freq_list.sort(key=lambda x: x[1], reverse=True)
+        freq_list = [(num, freq) for num, freq in counts.items()]
+        freq_list.sort(key=lambda x:x[1], reverse=True)
         top_k = [num for num, freq in freq_list[:k]]
         return top_k
-        
-        
