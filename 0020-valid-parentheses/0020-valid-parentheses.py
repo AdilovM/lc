263359f8434615d1c80618689245702eed6d50f8
Miras Adilov <miras9191@gmail.com>
2023-11-15 07:22:21 -0800

    Time: 33 ms (87.93%), Space: 16.4 MB (17.38%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,19 +1,21 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        closing_parens = {
-          ")" : "(",
-          "}" : "{",
-          "]" : "["
+        closed_parens = {
+            ")" : "(",
+            "}" : "{",
+            "]" : "["
         }
-
+        
         stack = []
+        
         for c in s:
-            if c not in closing_parens:
+            if c not in closed_parens:
                 stack.append(c)
             else:
-                if len(stack) == 0 or closing_parens[c] != stack[-1]:
+                if len(stack) == 0 or closed_parens[c] != stack[-1]:
                     return False
                 else:
                     stack.pop()
-    
         return not stack
+            
+                
