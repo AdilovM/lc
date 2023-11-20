2d72281f83aebcc0707472eda2fbe4393d5801ea
Miras Adilov <miras9191@gmail.com>
2023-11-20 15:02:57 -0800

    Time: 41 ms (96.07%), Space: 16.8 MB (65.39%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,21 +1,21 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # edge case
         if len(s) != len(t):
             return False
-        # count all chars in s and t
         s_chars = {}
         t_chars = {}
         
         for c in s:
             s_chars[c] = s_chars.get(c, 0) + 1
-            
         for c in t:
             t_chars[c] = t_chars.get(c, 0) + 1
-            
+        
         for c in s_chars:
-            if s_chars.get(c, 0) != t_chars.get(c, 0):
+            if c not in t_chars or t_chars[c] != s_chars[c]:
                 return False
         
         return True
+        
+        
+        
         
