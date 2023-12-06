aea532f65eb3e4a407be8917e3d871ebd8a0e5cb
Miras Adilov <miras9191@gmail.com>
2023-12-06 12:00:49 -0800

    Time: 180 ms (38.95%), Space: 25 MB (60.66%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.py
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.py
@@ -7,17 +7,19 @@
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
         
-        def dfs(node, path_sum):
-            if node:
-                if low <= node.val <= high:
-                    path_sum += node.val
-
-                if node.val > low:
-                    path_sum = dfs(node.left, path_sum)
-                if node.val < high:
-                    path_sum = dfs(node.right, path_sum)
-
-            return path_sum
+        path_sum = 0
+        stack = [root]
         
-        return dfs(root, 0)
-            
+        while stack:
+            current_node = stack.pop()
+            if current_node:
+                if low <= current_node.val <= high:
+                    path_sum += current_node.val
+
+                if current_node.val > low:
+                    stack.append(current_node.left)
+
+                if current_node.val < high:
+                    stack.append(current_node.right)
+                
+        return path_sum
