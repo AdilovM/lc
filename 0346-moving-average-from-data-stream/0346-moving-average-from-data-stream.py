efdc1adefcaf04bf2b15bd34886288bd082a3e69
Miras Adilov <miras9191@gmail.com>
2023-12-07 10:04:48 -0800

    Time: 99 ms (12.67%), Space: 20.1 MB (7.13%) - LeetHub

--- 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
+++ 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
@@ -2,13 +2,13 @@ class MovingAverage:
 
     def __init__(self, size: int):
         self.size = size
-        self. q = []
+        self.q = []
+
     def next(self, val: int) -> float:
         size, q = self.size, self.q
         q.append(val)
         window = sum(q[-size:])
-        return window / min(len(q), size )
-        
+        return window / min(len(q), size)
 
 
 # Your MovingAverage object will be instantiated and called as such:
