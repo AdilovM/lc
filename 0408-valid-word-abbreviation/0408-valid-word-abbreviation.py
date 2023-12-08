22f719a1e55e3dd99ca983308ecbbb2fed65f1e6
Miras Adilov <miras9191@gmail.com>
2023-12-08 14:41:55 -0800

    Time: 38 ms (66.12%), Space: 16.3 MB (26.26%) - LeetHub

--- 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
@@ -1,6 +1,20 @@
 class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
-        return bool(re.match(re.sub('([1-9]\d*)', r'.{\1}', abbr) + '$', word))
-            
-            
-                
+        i = j = 0
+        word_len, abbr_len = len(word), len(abbr)
+        
+        while i < word_len and j < abbr_len:
+            if word[i] == abbr[j]:
+                i += 1
+                j += 1
+            elif abbr[j] == "0":
+                return False
+            elif abbr[j].isnumeric():
+                k = j
+                while k < abbr_len and abbr[k].isnumeric():
+                    k += 1
+                i += int(abbr[j:k])
+                j = k
+            else:
+                return False
+        return i == word_len and j == abbr_len
