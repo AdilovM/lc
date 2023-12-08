fd1e84b9ec4c88bb2784e94c933883b42759c8f1
Miras Adilov <miras9191@gmail.com>
2023-12-08 10:23:28 -0800

    Time: 51 ms (55.41%), Space: 17.6 MB (50.49%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,19 +1,15 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
-        # count chars in s
+        # count char freq
         counts = collections.Counter(s)
-        # get max freq for buckets size
-        max_freq = max(counts.values())
-        # create a bucket. start index 1
-        buckets = [[] for _ in range(max_freq + 1)]
-        # fill the fucket
-        for c, i in counts.items():
-            buckets[i].append(c)
+        max_heap = []
+        heapq.heapify(max_heap)
+        for char in counts:
+            heapq.heappush(max_heap, (-counts[char], char))
+        res = ""
+        for i in range(len(max_heap)):
+            f, c = heapq.heappop(max_heap)
+            res += c * (-f)
             
-        # going from R to L build the string
-        str_builder = []
+        return res
         
-        for i in range(len(buckets) - 1, 0, -1):
-            for c in buckets[i]:
-                str_builder.append(c * i)
-        return "".join(str_builder)
