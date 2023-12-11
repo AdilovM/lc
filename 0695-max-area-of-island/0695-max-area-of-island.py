1cf55d75ebc4780b2236c59a0d07d4e56868638f
Miras Adilov <miras9191@gmail.com>
2023-12-11 11:43:21 -0800

    Time: 155 ms (9.90%), Space: 19.8 MB (35.26%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -4,22 +4,17 @@ class Solution:
             return 0
         ROWS, COLS = len(grid), len(grid[0])
         visited = set()
-        directions = [[0,1], [0,-1], [1,0], [-1,0]]
-        area = 0
+        
         def dfs(r,c):
-            
+            # if current node is water
             if r not in range(ROWS) or c not in range(COLS) or (r,c) in visited or grid[r][c] == 0:
                 return 0
-            if grid[r][c] == 1:
-                visited.add((r,c))
+            # if current node is island
+            visited.add((r,c))
             return 1 + dfs(r, c + 1) + dfs(r, c - 1) + dfs(r + 1, c) + dfs(r - 1, c)
         
+        area = 0
         for r in range(ROWS):
             for c in range(COLS):
                 area = max(area, dfs(r,c))
         return area
-                
-        
-        
-                
-            
