cbfbabdaab0dcc7658bc2acc9e3f9964edc9b5c3
Miras Adilov <miras9191@gmail.com>
2023-12-11 12:06:01 -0800

    Time: 367 ms (9.69%), Space: 24.3 MB (25.99%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,22 +2,31 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
+        
         ROWS, COLS = len(grid), len(grid[0])
         visited = set()
         islands = 0
-        directions = [[0,1], [0,-1], [1,0], [-1,0]]
         
-        def dfs(r,c):
-            if r not in range(ROWS) or c not in range(COLS) or (r,c) in visited or grid[r][c] == "0":
-                return
+        def bfs(r,c):
+            q = deque()            
+            q.append((r,c))
             visited.add((r,c))
-            for dr, dc in directions:
-                
-                dfs(r + dr, c + dc)
+            directions = [[0,1], [0,-1], [1,0],[-1,0]]
+            while q:
+                r, c = q.popleft()
+                for dr, dc in directions:
+                    row = r + dr
+                    col = c + dc
+                    if row in range(ROWS) and col in range(COLS) and (row, col) not in visited and grid[row][col] == "1":
+                        visited.add((row, col))
+                        q.append((row, col))
+                        
         for r in range(ROWS):
             for c in range(COLS):
                 if (r,c) not in visited and grid[r][c] == "1":
+                    bfs(r,c)
                     islands += 1
-                    dfs(r,c)
+                    
         return islands
-                
+        
+        
