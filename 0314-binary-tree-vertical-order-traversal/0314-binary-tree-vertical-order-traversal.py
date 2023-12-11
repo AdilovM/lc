ee86048a873f7a89e695a2a4b916ed2dbbb98f61
Miras Adilov <miras9191@gmail.com>
2023-12-11 14:23:19 -0800

    Time: 41 ms (59.90%), Space: 16.3 MB (38.69%) - LeetHub

--- 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.py
+++ 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.py
@@ -6,18 +6,17 @@
 #         self.right = right
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        # dict of lists
         column_table = defaultdict(list)
-        # create q and add (root, 0) as first element
+        print(column_table)
         q = deque([(root, 0)])
-        # while element in q
         while q:
-            # pop (node, column)
             node, column = q.popleft()
-            # if node exists add node.val at column_table[column]
             if node:
                 column_table[column].append(node.val)
-                # append node.left to left side from root
-                q.append((node.left, column - 1))
+                q.append((node.left, column -1))
                 q.append((node.right, column + 1))
+
         return [column_table[x] for x in sorted(column_table.keys())]
+                
+            
+            
