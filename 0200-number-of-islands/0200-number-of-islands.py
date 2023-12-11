e2d18aacfa57d56916d2bb966ab1bb59d3473ef7
Miras Adilov <miras9191@gmail.com>
2023-12-11 11:19:18 -0800

    Time: 358 ms (14.40%), Space: 27 MB (17.34%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,19 +3,21 @@ class Solution:
         if not grid:
             return 0
         ROWS, COLS = len(grid), len(grid[0])
-        visit = set()
+        visited = set()
         islands = 0
-        directions = [[0,1],[0,-1],[1,0],[-1,0]]        
+        directions = [[0,1], [0,-1], [1,0], [-1,0]]
         
         def dfs(r,c):
-            if r not in range(ROWS) or c not in range(COLS) or grid[r][c] == "0" or (r,c) in visit:
+            if r not in range(ROWS) or c not in range(COLS) or (r,c) in visited or grid[r][c] == "0":
                 return
-            visit.add((r,c))
+            visited.add((r,c))
             for dr, dc in directions:
+                
                 dfs(r + dr, c + dc)
         for r in range(ROWS):
             for c in range(COLS):
-                if grid[r][c] == "1" and (r,c) not in visit:
+                if (r,c) not in visited and grid[r][c] == "1":
                     islands += 1
                     dfs(r,c)
         return islands
+                
