8015a0ad403b988018396ef1f593c85ecf904f40
Miras Adilov <miras9191@gmail.com>
2023-12-12 11:44:49 -0800

    Time: 510 ms (74.68%), Space: 18.1 MB (5.28%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,20 +1,20 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         N = len(grid)
-        q = deque([(0,0,1)]) # r,c,length
-        visited = set((0,0))
+        q = deque([(0,0,1)])
         directions = [[0,1], [0,-1], [1,0], [-1,0],[1,1], [-1,-1], [1,-1], [-1,1]]
-
+        visited = set((0,0))
+        
         while q:
-            current_row, current_column, length = q.popleft()
-            if (min(current_row,current_column) < 0 or max(current_row,current_column) >= N or grid[current_row][current_column] == 1):
+            r, c, length = q.popleft()
+            if max(r,c) >= N or min(r,c) < 0 or grid[r][c] == 1:
                 continue
-            if current_row == N - 1 and current_column == N - 1:
+            if r == N - 1 and c == N - 1:
                 return length
             for dr, dc in directions:
-                if (current_row + dr, current_column + dc) not in visited:
-                    q.append((current_row + dr, current_column + dc, length + 1))
-                    visited.add((current_row + dr, current_column + dc))
+                if (r + dr, c + dc) not in visited:
+                    q.append((r + dr, c + dc, length + 1))
+                    visited.add((r + dr, c + dc))
         return -1
 
                 
