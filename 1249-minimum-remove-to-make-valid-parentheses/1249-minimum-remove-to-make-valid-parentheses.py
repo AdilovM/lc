8f36a5a20e70570fd064aba91a8d28064b1ab0bb
Miras Adilov <miras9191@gmail.com>
2023-12-11 19:45:49 -0800

    Time: 88 ms (68.82%), Space: 17.8 MB (86.02%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -5,14 +5,18 @@ class Solution:
         for idx, c in enumerate(s):
             if c not in "()":
                 continue
+            # if given char is "(" added to opening stack
             if c == "(":
                 opening.append(idx)
+            # if previous char wasnt "(" it was ")", add ")" to set
             elif not opening:
                 index_to_remove.add(idx)
+            # there is "(" in opening stack current ")", we pop opening stack
             else:
                 opening.pop()
+        # union index_to_remove set and opening set
         index_to_remove = index_to_remove.union(set(opening))
-        print(index_to_remove)
+        
         result = ""
         for idx, c in enumerate(s):
             if idx not in index_to_remove:
