5ac95602066651adca677461c76b70e49567e9da
Miras Adilov <miras9191@gmail.com>
2023-12-12 09:35:36 -0800

    Time: 40 ms (91.28%), Space: 17.5 MB (27.40%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -6,17 +6,10 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        """
-        recursively subtract current root.val from targetSum and if no more children left return if root.val equals to current targetSum.
-        if there are more children return next recursive call on left child or right child, either of them can return False or True
-        """
-        # base case 1
         if not root:
             return False
-        # if no more children left
         if not root.left and not root.right:
-            return root.val == targetSum
-        
+            return targetSum == root.val
         targetSum -= root.val
         
         return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
