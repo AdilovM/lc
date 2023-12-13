c9a8d07d37c289bae358037ccca4c1f03a0af055
Miras Adilov <miras9191@gmail.com>
2023-12-13 14:21:34 -0800

    Time: 487 ms (95.44%), Space: 29.4 MB (56.72%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,17 +1,34 @@
-class Solution:    
+class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        if not nums: 
-            return
-        pivot = random.choice(nums)
-        left =  [x for x in nums if x > pivot]
-        mid  =  [x for x in nums if x == pivot]
-        right = [x for x in nums if x < pivot]
-
-        L, M = len(left), len(mid)
+        """
+        simpe way:
+        1. order desc
+        2. return kth element
         
-        if k <= L:
-            return self.findKthLargest(left, k)
-        elif k > L + M:
-            return self.findKthLargest(right, k - L - M)
-        else:
-            return mid[0]
+        quickselect
+        1. choose pivot index, usually last index. swap elements so left side of pivot is less than kth element
+        and right side of pivot is greater than kth element
+        2. if pivot == k return pivot
+        3. if pivot < k, do quick select on right side, pivot = k - pivot and continue quickselect. 
+        example: k = 5 but pivot = 2, new pivot = 5 -2 = 3
+        4. if pivot > k, do quickselect on left side, we do search until pivot
+        curr
+        [3,2,1,5,6,4]
+        p           
+        """
+        min_value = min(nums)
+        max_value = max(nums)
+        count = [0] * (max_value - min_value + 1)
+
+        for num in nums:
+            count[num - min_value] += 1
+        
+        remain = k
+        # print(count)
+        for idx in range(len(count)-1, -1, -1):
+            remain -= count[idx]
+            if remain <= 0:
+                return idx + min_value # becase idx=num-min_value, then num=idx+min_value
+
+        return -1
+        
