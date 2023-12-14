ee4fbee5c03c06db5bf88a5043cce8dd99b4b1ec
Miras Adilov <miras9191@gmail.com>
2023-12-13 18:42:39 -0800

    Time: 1663 ms (30.35%), Space: 24 MB (70.41%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,22 +1,39 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        def counting_sort():
-            # Create the counting hash map.
-            counts = {}
-            # Find the minimum and maximum values in the array.
-            minVal, maxVal = min(nums), max(nums)
-            # Update element's count in the hash map.
-            for val in nums:
-                counts[val] = counts.get(val, 0) + 1
-
-            index = 0
-            # Place each element in its correct position in the array.
-            for val in range(minVal, maxVal + 1, 1):
-                # Append all 'val's together if they exist.
-                while counts.get(val, 0) > 0:
-                    nums[index] = val
-                    index += 1
-                    counts[val] -= 1
-
-        counting_sort()
+        return self.mergeSort(nums, 0, len(nums) - 1)
+    def mergeSort(self, nums, start, end):
+        if end - start + 1 <= 1:
+            return nums
+        mid = (start + end) // 2
+        
+        self.mergeSort(nums, start, mid)
+        self.mergeSort(nums, mid + 1, end)
+        
+        self.merge(nums, start, mid, end)
+        
         return nums
+    
+    def merge(self, nums, start, mid, end):
+        left_nums = nums[start: mid + 1]
+        right_nums = nums[mid + 1: end + 1]
+        
+        i, j, k = 0, 0, start
+        
+        while i < len(left_nums) and j < len(right_nums):
+            if left_nums[i] <= right_nums[j]:
+                nums[k] = left_nums[i]
+                i += 1
+            else:
+                nums[k] = right_nums[j]
+                j += 1
+            k += 1
+        
+        while i < len(left_nums):
+            nums[k] = left_nums[i]
+            i += 1
+            k += 1
+        while j < len(right_nums):
+            nums[k] = right_nums[j]
+            j += 1
+            k += 1
+                
