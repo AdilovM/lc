33e4d0981d1d7994c29647b00f7a1c7b07102689
Miras Adilov <miras9191@gmail.com>
2023-12-14 18:15:00 -0800

    Time: 572 ms (69.63%), Space: 33.9 MB (56.05%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
@@ -1,22 +1,12 @@
 class Solution:
     def findBuildings(self, heights: List[int]) -> List[int]:
-        """
-        traverse backwards and push each height into stack
-        pop the stack if current height is taller than top of the stack and append output array
-        """
         n = len(heights)
-        answer = []
         max_height = -1
+        output = []
         
         for current in reversed(range(n)):
             if max_height < heights[current]:
                 max_height = heights[current]
-                answer.append(current)
-                
-        answer.reverse()
-        return answer
-                
-                
-            
-        
-        
+                output.append(current)
+        output.reverse()
+        return output
