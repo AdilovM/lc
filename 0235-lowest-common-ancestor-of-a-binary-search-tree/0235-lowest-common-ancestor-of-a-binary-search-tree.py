c517ce2513788b9169305f570fc9bf5924b23b4d
Miras Adilov <miras9191@gmail.com>
2023-12-15 11:56:51 -0800

    Time: 81 ms (17.41%), Space: 20.9 MB (45.47%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,11 +7,18 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        curr = root
-        while curr:
-            if p.val > curr.val and q.val > curr.val:
-                curr = curr.right
-            elif p.val < curr.val and q.val < curr.val:
-                curr = curr.left
-            else:
-                return curr
+        if not root or root == p or root == q:
+            return root
+
+        # Recursively find LCA in the left subtree
+        left = self.lowestCommonAncestor(root.left, p, q)
+        # Recursively find LCA in the right subtree
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        # If both left and right are non-null, current node is the LCA
+        if left and right:
+            return root
+
+        # Otherwise, return the non-null value among left and right, 
+        # which indicates where a target node was found, or return None if neither was found
+        return left if left else right
