3a74bc15bd2226008f5cf1d786c421accdfd3cff
Miras Adilov <miras9191@gmail.com>
2023-12-18 11:48:51 -0800

    Time: 60 ms (26.35%), Space: 16.8 MB (78.63%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,21 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if (s and not t) or (not s and t):
-            return False
         if len(s) != len(t):
             return False
-
         count = [0] * 128
+        
         for char in s:
-            count[ord(char)] +=1
-
+            count[ord(char)] += 1
+        
         for char in t:
             count[ord(char)] -= 1
             if count[ord(char)] < 0:
                 return False
-        
+            
         return True
-        
-        
-        
-        
