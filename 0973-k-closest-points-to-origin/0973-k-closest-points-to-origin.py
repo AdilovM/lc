9596832021fde905228c20dc53354e3bb8cfe401
Miras Adilov <miras9191@gmail.com>
2023-12-19 09:00:39 -0800

    Time: 721 ms (40.81%), Space: 23.7 MB (6.72%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,15 +1,11 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         min_heap = []
-        
         for x, y in points:
-            d = x**2 + y**2
-            min_heap.append((d, x, y))    
-        
-        heapq.heapify(min_heap)
+            min_heap.append((x**2 + y**2, x, y))
+        heapq.heapify(min_heap)        
         res = []
         for i in range(k):
             d, x, y = heapq.heappop(min_heap)
             res.append([x,y])
         return res
-        
