afc14b11672ddeebe1fbe4c6d9f38b81464174d5
Miras Adilov <miras9191@gmail.com>
2023-12-18 17:24:14 -0800

    Time: 136 ms (37.30%), Space: 17 MB (31.18%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,21 +1,20 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        def check_palindrome(s, i, j):
-            while i < j:
-                if s[i] != s[j]:
-                    return False
-                i += 1
-                j -= 1
+        l, r = 0, len(s) - 1
+        
+        while l < r:
+            if s[l] != s[r]:
+                return self.is_palindrome(s, l + 1, r) or self.is_palindrome(s, l, r - 1)
             
-            return True
-
-        i = 0
-        j = len(s) - 1
-        while i < j:
-            # Found a mismatched pair - try both deletions
-            if s[i] != s[j]:
-                return check_palindrome(s, i, j - 1) or check_palindrome(s, i + 1, j)
-            i += 1
-            j -= 1
-
+            l += 1
+            r -= 1
+            
+        return True
+    
+    def is_palindrome(self, s, l, r):
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1
         return True
