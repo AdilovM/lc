7061bc4751331505321c47f07f55d6f0962f0c33
Miras Adilov <miras9191@gmail.com>
2023-12-20 09:41:40 -0800

    Time: 89 ms (67.92%), Space: 18.8 MB (17.46%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -1,26 +1,22 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
         index_to_remove = set()
-        opening = []
-        for idx, c in enumerate(s):
-            if c not in "()":
+        opening_stack = []
+        
+        for idx, char in enumerate(s):
+            if char not in "()":
                 continue
-            # if given char is "(" added to opening stack
-            if c == "(":
-                opening.append(idx)
-            # if previous char wasnt "(" it was ")", add ")" to set
-            elif not opening:
+            if char == "(":
+                opening_stack.append(idx)
+            elif not opening_stack:
                 index_to_remove.add(idx)
-            # there is "(" in opening stack current ")", we pop opening stack
             else:
-                opening.pop()
-        # union index_to_remove set and opening set
-        index_to_remove = index_to_remove.union(set(opening))
+                opening_stack.pop()
         
-        result = ""
-        for idx, c in enumerate(s):
+        index_to_remove = index_to_remove.union(set(opening_stack))
+        
+        res = ""
+        for idx, char in enumerate(s):
             if idx not in index_to_remove:
-                result += c
-        return result
-        
-        
+                res += char
+        return res
