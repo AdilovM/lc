b9b7686740f00b0461c550f3d0557627f022b970
Miras Adilov <miras9191@gmail.com>
2023-12-23 20:36:32 -0800

    Time: 108 ms (69.20%), Space: 17.8 MB (12.89%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -5,12 +5,10 @@ class Solution:
         while l < r:
             if s[l] != s[r]:
                 return self.is_palindrome(s, l + 1, r) or self.is_palindrome(s, l, r - 1)
-            
             l += 1
             r -= 1
-            
         return True
-    
+        
     def is_palindrome(self, s, l, r):
         while l < r:
             if s[l] != s[r]:
