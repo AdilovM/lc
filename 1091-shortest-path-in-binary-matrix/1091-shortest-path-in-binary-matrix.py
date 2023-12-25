564ef0cdeb805a46816255a7ca67011c6ae3780e
Miras Adilov <miras9191@gmail.com>
2023-12-25 14:18:09 -0800

    Time: 446 ms (95.17%), Space: 19.1 MB (5.21%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,21 +1,22 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         N = len(grid)
+        # deque will store set of r,c and current depth
         q = deque([(0,0,1)])
-        directions = [[0,1], [0,-1], [1,0], [-1,0],[1,1], [-1,-1], [1,-1], [-1,1]]
+        # hashset of visited cell
         visited = set((0,0))
+        # 8 directions in form of r and c
+        directions = [[0,1],[0,-1],[1,0],[-1,0],[1,1],[1,-1],[-1,1],[-1,-1]]
         
         while q:
-            r, c, length = q.popleft()
+            r, c, d = q.popleft()
             if max(r,c) >= N or min(r,c) < 0 or grid[r][c] == 1:
                 continue
             if r == N - 1 and c == N - 1:
-                return length
+                return d
             for dr, dc in directions:
-                if (r + dr, c + dc) not in visited:
-                    q.append((r + dr, c + dc, length + 1))
-                    visited.add((r + dr, c + dc))
+                row, col = r + dr, c + dc
+                if (row, col) not in visited:
+                    visited.add((row,col))
+                    q.append((row,col,d + 1))
         return -1
-
-                
-        
