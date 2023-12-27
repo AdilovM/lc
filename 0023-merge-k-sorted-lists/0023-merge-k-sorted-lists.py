a55d469cd990a5a4e65659ad50b714dba937e1d3
Miras Adilov <miras9191@gmail.com>
2023-12-26 20:08:37 -0800

    Time: 74 ms (97.69%), Space: 20.3 MB (31.42%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -8,32 +8,27 @@ class Solution:
         if not lists or len(lists) == 0:
             return None
         while len(lists) > 1:
-            mergedLists = []
+            mergedList = []
             for i in range(0, len(lists), 2):
                 l1 = lists[i]
                 l2 = lists[i + 1] if i < len(lists) - 1 else None
-                
-                mergedLists.append(self.mergeTwoLists(l1,l2))
-            lists = mergedLists
+                mergedList.append(self.mergeTwoLists(l1, l2))
+            lists = mergedList
         return lists[0]
-    
     
     def mergeTwoLists(self, l1, l2):
         head = ListNode()
-        tail = head
-        
+        curr = head
         while l1 and l2:
             if l1.val < l2.val:
-                tail.next = l1
+                curr.next = l1
                 l1 = l1.next
             else:
-                tail.next = l2
+                curr.next = l2
                 l2 = l2.next
-                
-            tail = tail.next
+            curr = curr.next
         if l1:
-            tail.next = l1
+            curr.next = l1
         elif l2:
-            tail.next = l2
-        
+            curr.next = l2
         return head.next
