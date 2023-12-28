5cb09a090ce41148ab49e812c506dc19e6f52f7e
Miras Adilov <miras9191@gmail.com>
2023-12-27 21:13:29 -0800

    Time: 600 ms (5.03%), Space: 25.1 MB (20.44%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -9,18 +9,18 @@ class Solution:
         def bfs(r,c):
             q = deque([(r,c)])
             visited.add((r,c))
-            directions = [[0,1], [0,-1],[1,0], [-1,0]]
-            
+            directions = [[1,0], [0,1], [-1,0], [0,-1]]
             while q:
                 r, c = q.popleft()
                 for dr, dc in directions:
                     row, col = r + dr, c + dc
-                    if (row,col) not in visited and row in range(ROWS) and col in range(COLS) and grid[row][col] == "1":
+                    if (row, col) not in visited and row in range(ROWS) and col in range(COLS) and grid[row][col] == "1":
                         visited.add((row, col))
                         q.append((row, col))
+                        
         for r in range(ROWS):
             for c in range(COLS):
                 if (r, c) not in visited and grid[r][c] == "1":
-                    bfs(r, c)
+                    bfs(r,c)
                     islands += 1
         return islands
