b2f7ef0539c16c65aa01fba99d2d1c3979a3b2f5
Miras Adilov <miras9191@gmail.com>
2023-12-27 19:34:16 -0800

    Time: 1199 ms (5.59%), Space: 28.3 MB (9.18%) - LeetHub

---
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        N = len(colors)
+        l = res = 0
+        for r in range(1, N):
+            if colors[l] == colors[r]:
+                if neededTime[l] < neededTime[r]:
+                    res += neededTime[l]
+                    l = r
+                else:
+                    res += neededTime[r]
+            else:
+                l = r
+        return res
