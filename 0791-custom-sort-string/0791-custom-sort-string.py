2b239140236258cceec9b98adccb5a3185e3ad9c
Miras Adilov <miras9191@gmail.com>
2023-12-28 12:04:31 -0800

    Time: 76 ms (6.61%), Space: 17.2 MB (7.99%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.py
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -1,13 +1,14 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
         count = Counter(s)
-        answer = []
+        result = []
         
         for c in order:
-            answer.append(c * count[c])
+            result.append(c * count[c])
             count[c] = 0
         
         for c in count:
-            answer.append(c * count[c])
-            
-        return "".join(answer)
+            result.append(c * count[c])
+        
+        
+        return "".join(result)
