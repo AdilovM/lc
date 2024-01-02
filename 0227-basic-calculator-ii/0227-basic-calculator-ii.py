f2e6df05a3e3362ae0aef68551f5d1c122d20786
Miras Adilov <miras9191@gmail.com>
2024-01-02 08:41:06 -0800

    Time: 217 ms (12.29%), Space: 19.9 MB (33.63%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,30 +1,34 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        length = len(s)
-        if length == 0:
+        N = len(s)
+        if N == 0:
             return 0
-
-        currentNumber = 0
-        lastNumber = 0
-        result = 0
-        sign = '+'
-
-        for i in range(length):
-            currentChar = s[i]
-            if currentChar.isdigit():
-                currentNumber = (currentNumber * 10) + int(currentChar)
-            
-            if (not currentChar.isdigit() and not currentChar.isspace()) or i == length - 1:
-                if sign == '+' or sign == '-':
-                    result += lastNumber
-                    lastNumber = currentNumber if sign == '+' else -currentNumber
-                elif sign == '*':
-                    lastNumber *= currentNumber
-                elif sign == '/':
-                    lastNumber = int(lastNumber / currentNumber)  # Integer division
-
-                sign = currentChar
-                currentNumber = 0
+        curr_result = 0
+        prev_result = 0
+        final_result = 0
+        sign = "+"
         
-        result += lastNumber
-        return result
+        
+        for i in range(N):
+            curr_char = s[i]
+            
+            if curr_char.isdigit():
+                curr_result = curr_result * 10 + int(curr_char)
+            
+            if (not curr_char.isdigit() and not curr_char.isspace()) or i == N - 1:
+                if sign in "+-":
+                    final_result += prev_result
+                    if sign == "+":
+                        prev_result = curr_result
+                    else:
+                        prev_result = -curr_result
+                elif sign == "*":
+                    prev_result *= curr_result
+                elif sign == "/":
+                    prev_result = int(prev_result / curr_result)
+                    
+                sign = curr_char
+                curr_result = 0
+        
+        final_result += prev_result
+        return final_result
