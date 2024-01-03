3e6b792ae0376128790b00c98519f782e6cb647a
Miras Adilov <miras9191@gmail.com>
2024-01-03 15:17:59 -0800

    Time: 30 ms (96.32%), Space: 17.3 MB (12.16%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -1,3 +1,10 @@
+"""
+Time complexity: O(m+n)
+We need to compare the two concatenations of length O(m+n), it takes O(m+n) time.
+We calculate the GCD using binary Euclidean algorithm, it takes log(m⋅n) time.
+To sum up, the overall time complexity is O(m+n).
+Space complexity: O(m+n) We need to compare the two concatenations of length O(m+n).
+"""
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
         if str1 + str2 != str2 + str1:
