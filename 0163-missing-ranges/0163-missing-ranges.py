67950a9099206ed3e63f91673cde27fcf6635e4f
Miras Adilov <miras9191@gmail.com>
2024-01-17 12:02:57 -0800

    Time: 47 ms (13.92%), Space: 17.3 MB (40.95%) - LeetHub

--- 0163-missing-ranges/0163-missing-ranges.py
+++ 0163-missing-ranges/0163-missing-ranges.py
@@ -1,8 +1,10 @@
 class Solution:
     def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[List[int]]:
-        res=[]
-        for a in nums+[upper+1]:
-            if a>lower:
-                res.append([lower,a-1])
-            lower=a+1
+        res = []
+        nums = [lower - 1] + nums + [upper + 1]  # Adding boundaries
+
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i - 1] + 1:  # Check if there is a gap
+                res.append([nums[i - 1] + 1, nums[i] - 1])
+
         return res
