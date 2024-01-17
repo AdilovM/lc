9f9d7e6404dd13d61236c540a4d99509b8ef5fa0
Miras Adilov <miras9191@gmail.com>
2024-01-17 10:44:10 -0800

    Time: 42 ms (31.20%), Space: 17.4 MB (19.25%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.py
+++ 0670-maximum-swap/0670-maximum-swap.py
@@ -1,21 +1,24 @@
 class Solution:
     def maximumSwap(self, num: int) -> int:
-        nums = list(str(num))
-        # from right to left, store the position of the greatest value that is greater than self
-        n = len(nums)
-        pos = [None for _ in range(n)]
-        pos[-1] = n - 1
-        
-        for i in reversed(range(n - 1)):  # O(N)
-            if nums[i] <= nums[pos[i + 1]]:
-                pos[i] = pos[i + 1]
-            else:
-                pos[i] = i
+        nums = list(str(num))  # Convert the number to a list of its digits
+        n = len(nums)  # Length of the number in terms of digits
+        pos = [None for _ in range(n)]  # Initialize an array to store positions
+        pos[-1] = n - 1  # The last digit is the greatest by default in its own position
 
+        # Traverse the digits from right to left
+        for i in reversed(range(n - 1)):  # O(N) - Iterate through the digits in reverse order
+            # Compare each digit with the greatest digit found so far to its right
+            if nums[i] <= nums[pos[i + 1]]:
+                pos[i] = pos[i + 1]  # If the right digit is greater, update the position
+            else:
+                pos[i] = i  # Otherwise, the current digit is the greatest so far for its position
+
+        # Traverse the digits from left to right to find the first digit that can be swapped
         for index, val in enumerate(nums):
+            # If a greater digit is found on the right, perform the swap
             if nums[pos[index]] > val:
-                right_index = pos[index]
-                nums[index], nums[right_index] = nums[right_index], nums[index]
-                break
-        
-        return int(''.join(nums))
+                right_index = pos[index]  # Get the index of the greater digit
+                nums[index], nums[right_index] = nums[right_index], nums[index]  # Swap the digits
+                break  # Break after the first swap as only one swap is needed
+
+        return int(''.join(nums))  # Convert the list of digits back to an integer and return
