d0b1c8bf917edeced063d8cd6679e51f688839c8
Miras Adilov <miras9191@gmail.com>
2024-01-17 16:34:23 -0800

    Time: 51 ms (86.16%), Space: 17.4 MB (50.19%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(start, end):
+            if start == end:
+                result.append(nums[:])
+                return
+            seen = set()
+            for i in range(start, end):
+                if nums[i] not in seen:
+                    seen.add(nums[i])
+                    nums[start], nums[i] = nums[i], nums[start]
+                    backtrack(start + 1, end)
+                    nums[start], nums[i] = nums[i], nums[start]
+
+        result = []
+        backtrack(0, len(nums))
+        return result
