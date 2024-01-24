f086ba5dfd1ecb7c86800fb66161e97d1ddc6e95
Miras Adilov <miras9191@gmail.com>
2024-01-23 16:10:53 -0800

    Time: 34 ms (79.01%), Space: 16.6 MB (60.41%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,15 +1,26 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        # Base case: Any number to the power of 0 is 1.
+        # Base case: Any number to the power of 0 is 1
         if n == 0:
             return 1
+        # Base case: 0 raised to any power is 0
+        if x == 0:
+            return 0
 
-        # If n is negative, work with its absolute value and invert the result.
+        # Handling negative n
         if n < 0:
             x = 1 / x
             n = -n
 
-        # Recursively calculate the power.
-        # If n is even, use the formula (x^n/2)^2.
-        # If n is odd, use x * (x^n/2)^2.
-        return self.myPow(x, n // 2) ** 2 if n % 2 == 0 else x * self.myPow(x, n // 2) ** 2
+        # Exponentiation by squaring
+        result = 1
+        while n > 0:
+            # If the n is odd, multiply the result with the x
+            if n % 2:
+                result *= x
+            # Square the base
+            x *= x
+            # Halve the n
+            n //= 2
+
+        return result
