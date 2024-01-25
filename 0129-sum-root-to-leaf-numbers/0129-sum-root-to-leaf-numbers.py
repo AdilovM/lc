c0578adbb82ea42c395c3c298b18aad96d662f65
Miras Adilov <miras9191@gmail.com>
2024-01-25 12:05:04 -0800

    Time: 38 ms (56.84%), Space: 16.6 MB (56.87%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -6,20 +6,44 @@
 #         self.right = right
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        def dfs(node, current_number):
-            if not node:
-                return 0
+        root_to_leaf = curr_number = 0
 
-            current_number = current_number * 10 + node.val
-            
-            # If it's a leaf node, return the current number
-            if not node.left and not node.right:
-                return current_number
+        while root:
+            # Check if the current node has a left child
+            if root.left:
+                # Finding the inorder predecessor of the current node
+                predecessor = root.left
+                steps = 1  # To count the number of steps to revert the current number later
+                # Finding the rightmost child of the left subtree or the already visited node
+                while predecessor.right and predecessor.right is not root:
+                    predecessor = predecessor.right
+                    steps += 1
 
-            # Recursively call for left and right children
-            return dfs(node.left, current_number) + dfs(node.right, current_number)
+                # Establish a temporary link from the predecessor to the current node
+                if predecessor.right is None:
+                    curr_number = curr_number * 10 + root.val  # Update the current number
+                    predecessor.right = root  # Make the temporary link
+                    root = root.left  # Move to the left child
+                else:
+                    # If there is a temporary link, it means we have visited the left subtree
+                    # Check if it's a leaf node to update the sum
+                    if predecessor.left is None:
+                        root_to_leaf += curr_number
+                    # Reverting the current number to its parent node's number
+                    for _ in range(steps):
+                        curr_number //= 10
+                    # Remove the temporary link and move to the right subtree
+                    predecessor.right = None
+                    root = root.right
+            else:
+                # Update the current number and move to the right child
+                curr_number = curr_number * 10 + root.val
+                # Check if it's a leaf node to update the sum
+                if root.right is None:
+                    root_to_leaf += curr_number
+                root = root.right
 
-        return dfs(root, 0)
+        return root_to_leaf
         
             
         
